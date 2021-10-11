21218d18403c1d6a2d19d957ac85e7bcabd3872e
LuPiJR <l.picker@student.tugraz.at>
2021-10-11 21:27:33 +0200

    introduced streamlit

--- main.py
+++ main.py
@@ -1,36 +1,52 @@
 import pandas as pd
 import yfinance as yf
-from bs4 import BeautifulSoup
-from pandas_datareader import data as pdr
+import streamlit as st
+import base64
 
-tickers_list = ['AAPL']
+nasdaq_tickers = pd.read_excel('name.xlsx')  
+tickers_list = nasdaq_tickers['Symbol']
 tickers_data = {}
-apple = yf.Ticker("AAPL")
-#apple = yf.Ticker("AAPL")
 
-#yf.pdr_override() # <== that's all it takes :-)
-
-# download dataframe
-#data = pdr.get_data_yahoo("SPY", start="2017-01-01", end="2017-04-30")
-#print(data)
+st.header('MarketData')
 
 
-for ticker in tickers_list:
-    ticker_object = yf.Ticker(ticker)
-    temp = pd.DataFrame.from_dict(ticker_object.info, orient='index')
-    temp.reset_index(inplace=True)
-    temp.columns = ['Attribute','Recent']
-    tickers_data[ticker] = temp
+data_of_interest = ['symbol','longName','website','targetMeanPrice','recommendationMean','regularMarketDayLow','regularMarketDayHigh']
+df = pd.DataFrame(columns=data_of_interest)
+selected_ticker = st.multiselect('Select tickers', tickers_list)
 
-combined_data = pd.concat(tickers_data)
-combined_data = combined_data.reset_index()
+st.text('Select ticker(s) or upload a csv with ticker(s) inside')
 
-del combined_data['level_1']
-combined_data.columns = ['Ticker','Attribute','Recent']
+data_file = st.file_uploader("Upload CSV",type=['csv'])
 
-print(combined_data)
-print(apple.financials)
+if data_file is not None:
+    nasdaq_tickers = pd.read_csv(data_file)
+    st.dataframe(nasdaq_tickers['Symbol'])
+    selected_ticker = nasdaq_tickers['Symbol'].tolist()
 
-#combined_data.to_excel('combined_data.xlsx')
-#recommendationMean
+
+def get_data_of_interest(data):
+    value_of_interest = {}
+    for name_of_interest in data_of_interest:
+        value_of_interest[name_of_interest] = data.get(name_of_interest)        
+    return value_of_interest
+
+
+@st.cache
+def convert_df(df):
+# IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
+if st.button('Get Data') and selected_ticker:
+    for ticker in selected_ticker:
+        ticker_object = yf.Ticker(ticker)
+        values_of_interest = get_data_of_interest(ticker_object.info)
+        df = df.append(values_of_interest,ignore_index=True)
+    st.dataframe(df)
+    csv = convert_df(df)
+    st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name='large_df.csv',
+    mime='text/csv',
+    )
 
